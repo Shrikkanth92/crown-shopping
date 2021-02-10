221b81a764f7444168a0c9e94f8b63246eb16a86
Shrikkanth Ramesh <shrikkanth.ramesh@unicosolution.com>
2021-02-10 08:49:38 -0600

    Adding routes to app using react router and navigating to pages using withRoutes HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
 import { HomePage } from './pages/homepage/homepage.component';
+
+const HatsPage = (props) => {
+  return (
+    <div>
+        "Hats Page";
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-        <HomePage></HomePage>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route exact path="/hats" component={HatsPage}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
